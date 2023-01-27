68ebf615f1a9562844a12b9e9842c143b0ca26a4
Cristian Beasley <123713404+Beaz202@users.noreply.github.com>
2023-01-27 15:55:59 +0000

    Got the Pyramid to display 2 and number enter in height

--- mario.js
+++ mario.js
@@ -1,27 +1,9 @@
 
-printPyramid(5);
 // TODO #1
 // Take in user input for the height]
 let height = prompt("Enter the Height: ");
-let space = " ";
-let brick = "#";
-let layer = space+space+brick+brick;
-
-for(let i = 1; i<=height; i++){
-  for(let x = 0; x<height-1; x++){
-    space += " ";
-  }
-  for(let s = 0; s < i; s++){ 
-    space += '*';
-  }
-  space += "/n";
-}
-console.log(space);
-
-
-
+printPyramid(parseInt(height));
 /*
-
  * printPyramid
  *
  * Prints to the console a pyramid of '#' characters of the specified height
@@ -35,8 +17,24 @@ console.log(space);
 function printPyramid(height) {
   console.log("Uh oh... the pyramid is under construction.");
   console.log("Check back soon, our developers are hard at work as we speak!");
-
+  let space = " ";
+  let brick = "#";
+  let string = "";
+  let layer = space+space+brick+brick;
+  for(let i = 1; i <= height; i++){
+    
+    for(x = 0; x < height-i; x++){
+    string += space;
+    }
+    
+    for(let s = 0; s < i+1; s++){ 
+    string += brick;
+    }
+    
+    string += "\n";
+  }
   // TODO #1
   // print that pyramid!
+console.log(string);
 
 }
